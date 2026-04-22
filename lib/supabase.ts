b8842f1e177dcb9aab31bbc83eb6b082b552f11c
Denis Mutugi <aweolar@gmail.com>
2026-04-22 10:54:18 +0300

    improved routing

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
 // We use the '!' at the end to tell TypeScript that we promise these environment variables exist.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Upgraded to use the SSR Browser Client so it automatically sets cookies!
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
+
